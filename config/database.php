a6371ad8d6f69732229c42ab6a60332c6564c0a7
Oğuzhan Cerit <ogzhncrt@gmail.com>
2021-12-08 22:18:32 +0300

    Successful database connection

---
+++ config/database.php
@@ -0,0 +1,40 @@
+<?php
+
+class Database {
+    
+    public $conn;
+
+    /**
+     * @author Oğuzhan Cerit
+     * 
+     * This function will provide connection 
+     * to the provide`and return the database object.
+     *
+     * @param no-param
+     *
+     * @since 0.0.1
+     *
+     */
+    public function set_connection(){
+  
+        $this->conn = null;
+  
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }
+        catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+  
+        return $this->conn;
+    }
+
+
+    private $host     = "localhost";
+    private $db_name  = "bookapi";
+    private $username = "root";
+    private $password = "";
+}
+
+?>
